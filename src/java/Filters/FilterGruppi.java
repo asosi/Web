0d9787635b6c0e5d37eb03db31c3e1d91fcb5382
CaoS <sosi.andrea@gmail.com>
2014-01-03 14:26:34 +0100

    Update filter gruppi e XML

--- src/java/Filters/FilterGruppi.java
+++ src/java/Filters/FilterGruppi.java
@@ -1,6 +1,10 @@
 package Filters;
 
+import DB.DBConnect;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -13,20 +17,67 @@ import javax.servlet.http.HttpSession;
 
 public class FilterGruppi implements Filter{
     
+  String ip;
+    
   public void init(FilterConfig arg0) throws ServletException {  }
 
   public void doFilter(ServletRequest request, ServletResponse response,
       FilterChain chain) throws IOException, ServletException {
       
-      //faccio qualcosa prima di chiamare la servlet Home 
-      //tipo posso controllare che l'utente sia loggato e controllo la sessione
+      
+      boolean trovato = false;
       try{
-         HttpSession session = ((HttpServletRequest) request).getSession();
-               
+          
+        HttpSession session = ((HttpServletRequest) request).getSession();       
         HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
         
         if(session != null && session.getAttribute("idUser")!= null){
-            chain.doFilter(request, response);
+            
+            PrintWriter out = response.getWriter();
+            ip = request.getLocalAddr();
+            DBConnect db = new DBConnect(out,ip);
+            int id;
+            synchronized(session){id = (Integer) session.getAttribute("idUser");}
+            
+            // seleziono tutti i gruppi come table
+            PreparedStatement ps = db.conn.prepareStatement("SELECT * from groups where id_owner = ?");
+            ps.setInt(1, id);
+            ResultSet rs = db.Query(ps,out);
+            
+            while(rs.next()){
+                String idgruppo = rs.getString("id");
+                
+                if(req.getRequestURL().toString().endsWith(idgruppo)){               
+                    trovato = true;
+                }
+            }
+            // chiudo resultset
+            rs.close();
+            
+            // seleziono tutti i gruppi come table 1
+            PreparedStatement ps2 = db.conn.prepareStatement("SELECT * from  groups join users_groups on(groups.id = users_groups.id_groups) where users_groups.id_users = ? and active = 1");
+            ps2.setInt(1, id);
+            ResultSet rs2 = db.Query(ps2,out);
+            
+            while(rs2.next()){
+                String idgruppo = rs2.getString("id");
+                
+                if(req.getRequestURL().toString().endsWith(idgruppo)){               
+                    trovato = true;
+                }
+            }
+            // chiudo resultset2
+            rs2.close();
+            
+            // chiudo la connessione con il DB
+            db.DBClose();
+            if(trovato == true){
+                chain.doFilter(request, response);
+            }else{
+                res.sendRedirect("Home");
+            }
+            
         } else {
             res.sendRedirect("index.html");
         }
@@ -35,10 +86,6 @@ public class FilterGruppi implements Filter{
           
       }   
   }
-  
-  //public String[] ReturnGruppi(){
-  //    String[] gruppi
-  //}
   
   @Override
   public void destroy() {  }
