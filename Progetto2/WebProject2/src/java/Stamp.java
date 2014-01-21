8e6e24abe96c178bb470191f7c537a33103e6225
empy92 <empy92@hotmail.it>
2014-01-21 11:37:38 +0100

    Add Stamp

---
+++ Progetto2/WebProject2/src/java/Stamp.java
@@ -0,0 +1,105 @@
+
+import DB.DBConnect;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author davide
+ */
+public class Stamp {
+    String ip;
+    
+    public Stamp(HttpServletRequest request){
+        ip = request.getLocalAddr();
+    }
+    
+    public ArrayList<String> Name(int id, PrintWriter out){
+        ArrayList<String> result = new ArrayList<String>();
+        DBConnect db = new DBConnect(out,ip);
+        
+        try{
+            PreparedStatement ps = db.conn.prepareStatement("SELECT name from users where id = ?");            
+            ps.setInt(1, id);
+            
+            ResultSet rs = db.Query(ps,out);
+            
+             while(rs.next()){
+                 result.add("<li class=\"dropdown\">");
+                 result.add("<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">"+rs.getString("name")+"<b class=\"caret\"></b></a>");
+             }
+        }
+        catch(SQLException e){}
+        db.DBClose();
+        return result;
+    }
+
+     private ArrayList<String> Table(int id, PrintWriter out){
+         ArrayList<String> result = new ArrayList<String>();
+         DBConnect db = new DBConnect(out,ip);
+        
+        try{
+            PreparedStatement ps = db.conn.prepareStatement("SELECT id,name,surname,avatar from users where id <> ? order by surname,name");            
+            ps.setInt(1, id);
+            
+            ResultSet rs = db.Query(ps,out);
+            
+            int contatore = 0;
+            
+             while(rs.next()){
+                 contatore++;
+                 
+                 result.add("<tr id='tr"+contatore+"'>");
+                 result.add("<td>"+rs.getString("id")+"</td>");
+                 result.add("<td>\n" +
+"                                <img class='table' src='"+rs.getString("avatar")+"' />"+rs.getString("surname")+" "+rs.getString("name")+"\n" +
+"                            </td>");
+                 result.add("<td>\n" +
+"                                <form>\n" +
+"                                    <div class='form-group'>\n" +
+"                                        <button class='btn btn-primary' name='btnSend"+contatore+"' id='"+contatore+"' onclick='Send(id)' type='button'>Send Invitation</button>\n" +
+"                                        <button disabled='disabled' class='btn btn-danger' id='"+contatore+"' name='btnCancel"+contatore+"' onclick='Cancel(id)' type='button'>Cancel</button>\n" +
+"                                    </div>\n" +
+"                                </form>\n" +
+"                            </td>");
+                 result.add("");
+                 result.add("");
+                 result.add("");
+             }
+        }
+        catch(SQLException e){}
+        db.DBClose();
+        return result;
+    }
+    
+    private ArrayList<String> EditAvatar(int id, PrintWriter out){
+        ArrayList<String> result = new ArrayList<String>();
+        DBConnect db = new DBConnect(out,ip);
+        
+        try{
+            PreparedStatement ps = db.conn.prepareStatement("SELECT name,surname,avatar from users where id = ?");
+            ps.setInt(1, id);
+
+            ResultSet rs = db.Query(ps,out);
+            
+             while(rs.next()){
+                 result.add("<h1>"+rs.getString("surname")+" "+rs.getString("name")+"</h1>");
+                 result.add("<img id='avatar' class=\"img-circle\" data-src=\"holder.js/200x200\"\" style=\"width: 200px; height: 200px;\" src='"+rs.getString("avatar")+"'/>");
+             }
+        }
+        catch(SQLException e){}
+        
+        db.DBClose();
+        return result;
+    }
+}
